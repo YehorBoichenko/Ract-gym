0ad620627f38f8c6100462715f3bf78411f79ef4
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-12-02 14:46:20 +0200

    set up project

---
+++ react-gym/src/components/Hero/Hero.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-hero">
+        <Header />
+        <div className="best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              Here we will help you to shape your ideak body and live up your
+              life to fullest
+            </span>
+          </div>
+        </div>
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>Expert Coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>Members Joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>Fitness Programs</span>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-hero"></div>
+    </div>
+  );
+};
+
+export default Hero;
