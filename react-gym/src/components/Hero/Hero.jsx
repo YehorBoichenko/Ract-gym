5e126861406b2f584bc0273a5083905d5619d215
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-12-05 17:33:33 +0200

    some progress bugfix needed and stylefix

--- react-gym/src/components/Hero/Hero.jsx
+++ react-gym/src/components/Hero/Hero.jsx
@@ -6,13 +6,21 @@ import Hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 
+import { motion } from "framer-motion";
+
 const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-hero">
         <Header />
         <div className="best-ad">
-          <div></div>
+          <motion.div
+            initial={{ left: "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
           <span>The best fitness club in the town</span>
         </div>
         <div className="hero-text">
@@ -51,21 +59,38 @@ const Hero = () => {
       </div>
       <div className="right-hero">
         <button className="btn">Join Now</button>
-        <div className="heart-rate">
+        <motion.div
+          initial={{ right: "-1rem" }}
+          transition={transition}
+          whileInView={{ right: "4rem" }}
+          className="heart-rate"
+        >
           <img src={Heart} alt="" />
           <span>Heart Rate</span>
           <span>120 bpm</span>
-        </div>
+        </motion.div>
         <img src={Hero_image} alt="" className="hero-image" />
-        <img src={Hero_image_back} alt="" className="hero-image-back" />
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem" }}
+          transition={transition}
+          src={Hero_image_back}
+          alt=""
+          className="hero-image-back"
+        />
 
-        <div className="calories">
+        <motion.div
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+          className="calories"
+        >
           <img src={Calories} alt="" />
           <div>
             <span>Calories Burned</span>
             <span>220 kcal</span>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
